2c978386bfbc2e9f1abd75cec6d469c3944744a8
gurdeep7 <90665311+gurdeep7@users.noreply.github.com>
2021-11-23 17:32:19 +0530

    progress

--- assignment2/app.js
+++ assignment2/app.js
@@ -2,31 +2,80 @@ const express = require("express")
 
 const app = express()
 
-const authors = require("./authors.json")
+const books = require("./books.json")
 
 app.use(express.json())
 
 
 const logger = (req,res,next) =>{
+    req.name  = "Gurdeep Singh"
 
-    console.log(req.method)
-
-    next();
-
+    
+   next()
 }
 
-app.get("/",(req,res)=>{
+app.get("/",logger,(req,res)=>{
   
-    res.send(authors)
+    res.send(books)
 })
 
-app.post("/books",(req,res)=>{
+app.get("/books/:id",logger,(req,res)=>{
+  
+    const specifc_book = books.filter(books => books.id == req.params.id)
 
-    const new_authors = [...authors,req.body]
+    var obj = {
+        "api_requested_by" : req.name,
+        "book" : specifc_book
+    }
+    res.send(obj)
 
-    res.send(new_authors)
+
 })
 
+app.patch("/books/:id",logger,(req,res)=>{
+
+const new_books = books.map((book)=>{
+   
+if(+req.params.id == book.id){
+    if(req?.body?.author) book.author = req.body.author
+    if(req?.body?.publish_year) book.publish_year = req.body.publish_year
+
+    return book
+}
+return book
+})
+var obj = {
+    "api_requested_by" : req.name,
+    "books" : new_books
+}
+res.send(obj)
+})
+
+
+app.post("/books",logger,(req,res)=>{
+
+    const new_books = [...books,req.body]
+
+    var obj = {
+        "api_requested_by" : req.name,
+        "books" : new_books
+    }
+    res.send(obj)
+})
+
+
+app.delete("/books:id",logger,(req,res)=>{
+  
+    const remaining_books = books.filter(books => books.id != req.params.id)
+
+    var obj = {
+        "api_requested_by" : req.name,
+        "books" : remaining_books
+    }
+    res.send(obj)
+
+
+})
 app.listen(2345,function(){
     console.log("listening on port 2345")
 })
