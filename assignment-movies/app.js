421a1f73e122ba5867b55ab48f4ce09b1d564d1f
gurdeep7 <90665311+gurdeep7@users.noreply.github.com>
2021-11-26 08:28:42 +0530

    done

---
+++ assignment-movies/app.js
@@ -0,0 +1,83 @@
+const e = require("express")
+const express = require("express")
+
+const mongoose = require("mongoose")
+
+const connect = () =>{
+    return mongoose.connect("mongodb://127.0.0.1:27017/movie")
+}
+
+const moviesSchema = new mongoose.Schema({
+    id : {type: Number, required:true, unique: true},
+
+    movie_name : {type: String, required: true},
+
+    movie_name : {type: String, required: false},
+
+    year:{type: Number, required:true},
+
+    budget:{type:Number, required: true}
+},{
+    versionKey:false,
+    timestamps:true
+})
+  
+const movie = mongoose.model("movie", moviesSchema)
+
+const app = express()
+
+app.use(express.json())
+
+app.post("/movies",async(req,res)=>{
+try{
+    const Movie = await movie.create(req.body)
+
+    res.status(201).send(Movie);
+}catch (err){
+    res.status(500).json({status: err.message})
+}
+})
+
+app.get("/", async(req,res)=>{
+try {
+    const Movie = await movie.find().lean().exec();
+
+    res.status(201).send(Movie);
+} catch (err) {
+    res.status(500).json({status: err.message});
+}
+});
+
+app.get("/movies:id", async(req,res)=>{
+    try {
+        const Movie = await movie.find({id:{$eq:req.params.id}}).lean().exec();
+    
+        res.status(201).send(Movie);
+    } catch (err) {
+        res.status(500).json({status: err.message});
+    }
+    });
+
+app.patch("/movies/:id", async(req,res)=>{
+try{
+    const Movie = await movie.findByIdAndUpdate(req.params.id,req.body,{new:true}).lean().exec()
+    res.status(201).send(Movie);
+} catch (err) {
+    res.status(500).json({status: err.message});
+}
+})
+
+app.delete("/movies/:id", async(req,res)=>{
+    try{
+        const Movie = await movie.findByIdAndDelete(req.params.id).lean().exec()
+        res.status(201).send(Movie);
+    } catch (err) {
+        res.status(500).json({status: err.message});
+    }
+    })
+    
+app.listen(2345, async function(){
+await connect()
+console.log("Listening to port number 2345")
+}) 
+
