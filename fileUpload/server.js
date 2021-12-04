c96b257ad7295685e095557d4c5adb15555d88e0
gurdeep7 <90665311+gurdeep7@users.noreply.github.com>
2021-12-04 19:22:49 +0530

    progess

---
+++ fileUpload/server.js
@@ -0,0 +1,11 @@
+const app = require("./index")
+
+const connect = require("./config/db")
+
+app.listen(2345, async function(){
+
+    await connect()
+
+    console.log("Listening on port 2345")
+})
+
