fb2d8626a5e6c49a0511023ed91b9005aa096856
gurdeep7 <90665311+gurdeep7@users.noreply.github.com>
2021-12-13 13:53:19 +0530

    done

--- redis/controllers/weather.controller.js
+++ redis/controllers/weather.controller.js
@@ -8,13 +8,14 @@ router.get("", async(req,res)=>{
     try{redis.get("weather_forecasts",async function(err,forecasts){
         if(err) console.log(err);
 
-        if(forecasts) return res.status(200).send(JSON.parse(forecasts))
+        if(forecasts){
+             return res.status(200).send(JSON.parse(forecasts))}
 
         const forecast = await weatherForecast.find().lean().exec()
 
         redis.set("weather_forecasts", JSON.stringify(forecast))
 
-    return res.status(200).json({db_forecast:forecast})
+    return res.status(201).json({db_forecast:forecast})
     }) }catch(e){
         res.status(500).json({status: e.message})
     }
@@ -28,12 +29,51 @@ try{
 
         const forecasts = await weatherForecast.find().lean().exec()
 
-        redis.set("weather_forecasts", JSON.stringify(forecast))
+        redis.set("weather_forecasts", JSON.stringify())
 
     return res.status(200).send(forecast)
 }catch(e){
     res.status(500).json({status: e.message})
 }
     
+})
+
+router.get("/:id", async(req,res)=>{
+    
+    try{redis.get(`weather_forecasts.${req.params.id}`,async function(err,forecasts){
+        if(err) console.log(err);
+
+        if(forecasts){
+             return res.status(200).send(JSON.parse(forecasts))}
+
+        const forecast = await weatherForecast.findById(req.params.id).lean().exec()
+
+        redis.set(`weather_forecasts.${req.params.id}`, JSON.stringify(forecast))
+
+    return res.status(201).json({db_forecast:forecast})
+    }) }catch(e){
+        res.status(500).json({status: e.message})
+    }
+})
+
+router.patch("/:id",async (req,res)=>{
+    const forecasts = await weatherForecast.findByIdAndUpdate(req.params.id, req.body, {new:true})
+
+    redis.set(`weather_forecasts.${req.params.id}`, JSON.stringify(forecasts))
+
+    const forecast =await weatherForecast.find().lean().exec()
+
+    redis.set(`weather_forecasts`, JSON.stringify(forecast))
+    res.status(200).send(forecasts)
+})
+router.delete("/:id",async (req,res)=>{
+    const forecasts = await weatherForecast.findByIdAndDelete(req.params.id, {new:true})
+
+    redis.hmset(`weather_forecasts.${req.params.id}`, JSON.stringify(forecasts))
+
+    const forecast =await weatherForecast.find().lean().exec()
+
+    redis.set(`weather_forecasts`, JSON.stringify(forecast))
+    res.status(200).send(forecasts)
 })
 module.exports = router
